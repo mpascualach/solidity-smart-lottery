2cc6f2c1f7a28a44be8168a688e7c06dc246c739
mpascualach <miguelpacheson@hotmail.es>
2023-07-24 19:55:39 +0200

    writing tests

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,11 +4,27 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+import "hardhat/console.sol";
 
+error Raffle__UpkeepNotNeeded(
+    uint256 currentBalance,
+    uint256 numPlayers,
+    uint256 raffleState
+);
 error Raffle__TransferFailed();
 error Raffle__SendMoreToEnterRaffle();
+error Raffle__RaffleNotOpen();
 
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    /* Type declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+    /* State variables */
+    // Chainlink VRF Variables
+
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
@@ -16,26 +32,85 @@ contract Raffle is VRFConsumerBaseV2 {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
+    uint256 private immutable i_interval;
     uint256 private immutable i_entranceFee;
+    uint256 private s_lastTimeStamp;
     address private s_recentWinner;
     address payable[] private s_players;
+    RaffleState private s_raffleState;
 
-    event RequestedRaffleWinner(uint256 indexed requestId);
+    event RequestRaffleWinner(uint256 indexed requestId);
     event RaffleEnter(address indexed player);
     event WinnerPicked(address indexed player);
 
     constructor(
         address vrfCoordinatorV2,
         uint64 subscriptionId,
-        bytes32 gasLane, // keyHash
+        bytes32 gasLane,
+        uint256 interval,
         uint256 entranceFee,
         uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
+        i_interval = interval;
         i_subscriptionId = subscriptionId;
         i_entranceFee = entranceFee;
+        s_raffleState = RaffleState.OPEN;
         i_callbackGasLimit = callbackGasLimit;
+    }
+
+    /**
+     * @dev The function that Chainlink Keeper nodes call
+     * they look for `upkeepNeeded` returning False
+     * the following should be true if it is to return true:
+     * 1. Enough time has passed between raffle runs
+     * 2. The lottery is open.
+     * 3. The contract has enough ETH
+     * 4. Your subscription is funded with LINK
+     *
+     */
+
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory /* performData */)
+    {
+        bool isOpen = RaffleState.OPEN == s_raffleState;
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = s_players.length > 0;
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers);
+        return (upkeepNeeded, "0x0");
+    }
+
+    /**
+     * @dev Once `checkUpkeep` returns `true`, this function is triggered
+     * and it kicks off a Chainlink VRF call to select a random winner.
+     */
+
+    function performUpkeep(bytes calldata /* performData */) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        // require(upkeepNeeded, "Upkeep not needed")
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestRaffleWinner(requestId);
     }
 
     function enterRaffle() public payable {
@@ -43,6 +118,9 @@ contract Raffle is VRFConsumerBaseV2 {
         // require(s_raffleState == RaffleState.OPEN, "Raffle is not open");
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
         }
         s_players.push(payable(msg.sender));
         // Emit an event when we update a dynamic array or mapping
@@ -79,5 +157,17 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
     }
 }
---
+++ test/unit/RaffleTest.js
@@ -0,0 +1,82 @@
+const { assert, expect } = require("chai");
+const { network, deployments, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../../helper-hardhat-config");
+
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("Raffle Unit Tests", function () {
+      let raffle,
+        raffleContract,
+        vrfCoordinatorV2Mock,
+        raffleEntranceFee,
+        interval,
+        player; // , deployer
+
+      beforeEach(async () => {
+        accounts = await ethers.getSigners(); // could also do with getNamedAccounts
+        //   deployer = accounts[0]
+        player = accounts[1];
+        await deployments.fixture(["mocks", "raffle"]); // Deploys modules with the tags "mocks" and "raffle"
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock"); // Returns a new connection to the VRFCoordinatorV2Mock contract
+        raffleContract = await ethers.getContract("Raffle"); // Returns a new connection to the Raffle contract
+        raffle = raffleContract.connect(player); // Returns a new instance of the Raffle contract connected to player
+        raffleEntranceFee = await raffle.getEntranceFee();
+        interval = await raffle.getInterval();
+      });
+
+      describe("constructor", function () {
+        it("initializes the raffle correctly", async () => {
+          // Ideally, we'd separate these out so that only 1 assert per "it" block
+          // And ideally, we'd make this check everything
+          const raffleState = (await raffle.getRaffleState()).toString();
+          // Comparisons for Raffle initialization:
+          assert.equal(raffleState, "0");
+          assert.equal(
+            interval.toString(),
+            networkConfig[network.config.chainId]["keepersUpdateInterval"]
+          );
+        });
+      });
+
+      describe("enterRaffle", function () {
+        it("reverts when you don't pay enough", async () => {
+          await expect(raffle.enterRaffle()).to.be.revertedWith(
+            // is reverted when not paid enough or raffle is not open
+            "Raffle__SendMoreToEnterRaffle"
+          );
+        });
+        it("records player when they enter", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          const contractPlayer = await raffle.getPlayer(0);
+          assert.equal(player.address, contractPlayer);
+        });
+        it("emits event on enter", async () => {
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.emit(
+            // emits RaffleEnter event if entered to index player(s) address
+            raffle,
+            "RaffleEnter"
+          );
+        });
+        it("doesn't allow entrance when raffle is calculating", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          // for a documentation of the methods below, go here: https://hardhat.org/hardhat-network/reference
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.request({ method: "evm_mine", params: [] });
+          // we pretend to be a keeper for a second
+          await raffle.performUpkeep([]); // changes the state to calculating for our comparison below
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.be.revertedWith(
+            // is reverted as raffle is calculating
+            "Raffle__RaffleNotOpen"
+          );
+        });
+      });
+    });
